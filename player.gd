d52fe2ffc1be90fe1afa4ea46c6b5558491a0b0f
Eero Häihälä <eero.haihala@gmail.com>
2025-01-03 12:01:56 +0200

    Rotate player character to face forward

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+var health: int = 10
 @export var speed: float = 1000
+@export var rotation_speed: float = 0.2
 
 func _physics_process(delta: float) -> void:
 	var movement_input = Vector2(
@@ -10,3 +12,14 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = movement_input * speed
 	move_and_slide()
+	
+	if not velocity.is_zero_approx():
+		var look_dir = Vector2(cos(rotation), sin(rotation))
+		var angle = look_dir.angle_to(velocity)
+			
+		if abs(angle) < rotation_speed:
+			look_at(position+velocity)
+		else:
+			# Gradually turn
+			var angle_sign = 1 if angle > 0 else -1
+			rotate(angle_sign * rotation_speed)
