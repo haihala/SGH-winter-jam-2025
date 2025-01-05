7ef0da2de33619560730872edebc58fe6e8b58fc
Eero Häihälä <eero.haihala@gmail.com>
2025-01-05 15:32:47 +0200

    Improve waveform generation script
    It can now do specific notes on the same track

--- intermediate-assets/scripts/gen_audio.py
+++ intermediate-assets/scripts/gen_audio.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+import functools
+import sys
 import numpy as np
 from scipy.io.wavfile import write
 
@@ -12,36 +14,33 @@ if not os.path.exists(TARGET):
     os.mkdir(TARGET)
 
 
-def make_wave(path, length, waveform):
+def make_wave(length, waveform):
     t = np.linspace(0, length, int(length * AUDIO_RATE), dtype=np.float32)
-    data = waveform(t)
-    write(f"{TARGET}/{path}", AUDIO_RATE, data)
+    return waveform(t)
 
 
-def make_flat_wave(path, length=1, freq=440):
-    make_wave(
-        path,
+def make_flat_wave(length=1, freq=440):
+    return make_wave(
         length,
         lambda t: np.sin(2 * np.pi * freq * t)
     )
 
 
-def make_pointy_wave(path, length=1, freq=440):
-    ramp_up = 0.1
+def make_pointy_wave(length=1, freq=440):
+    ramp_up = 0.1 * length
     ramp_down = length - ramp_up
 
     up = np.linspace(0.0, 1.0, int(ramp_up * AUDIO_RATE), dtype=np.float32)
     down = np.linspace(1.0, 0.0, int(ramp_down*AUDIO_RATE), dtype=np.float32)
     amp = np.concatenate((up, down))
 
-    make_wave(
-        path,
+    return make_wave(
         length,
         lambda t: np.multiply(np.sin(2 * np.pi * freq * t), amp)
     )
 
 
-notes = {
+NOTES = {
     "c": 16.35,
     "d": 17.32,
     "e": 18.35,
@@ -51,10 +50,44 @@ notes = {
     "b": 30.87
 }
 
-for octave in range(9):
-    for (note, base) in notes.items():
-        freq = base * (2**octave)
-        make_flat_wave(
-            f"flat-octave{octave-1}-{note}.wav", length=0.5, freq=freq)
-        make_pointy_wave(
-            f"pointy-octave{octave-1}-{note}.wav", length=0.5, freq=freq)
+
+length = float(sys.argv[1])
+if sys.argv[2] == "--all":
+    for octave in range(9):
+        for (note, base) in NOTES.items():
+            freq = base * (2**octave)
+
+            flat = make_flat_wave(length, freq)
+            path = f"{TARGET}/flat-octave{octave-1}-{note}.wav"
+            write(path, AUDIO_RATE, flat)
+
+            pointy = make_pointy_wave(length, freq)
+            path = f"{TARGET}/pointy-octave{octave-1}-{note}.wav"
+            write(path, AUDIO_RATE, pointy)
+else:
+    parsed_notes = [
+        (
+            NOTES[i[0]],
+            float(i[1:]),
+        )
+        for i in sys.argv[2:]
+    ]
+
+    frequencies = [
+        base * (2**octave)
+        for (base, octave) in parsed_notes
+    ]
+
+    for path_bit, fun in [
+        ('flat', make_flat_wave),
+        ('pointy', make_pointy_wave)
+    ]:
+        data = np.divide(
+            functools.reduce(
+                np.add,
+                [fun(length, freq) for freq in frequencies]
+            ),
+            len(frequencies)
+        )
+        path = f"{TARGET}/{path_bit}-{'-'.join(sys.argv[2:])}.wav"
+        write(path, AUDIO_RATE, data)
