c4c1cca0082c971f583ec5400c5b7bb63d84c45f
Eero Häihälä <eero.haihala@gmail.com>
2025-01-05 16:57:54 +0200

    Add tutorial

--- godot-project/menus/main_menu.gd
+++ godot-project/menus/main_menu.gd
@@ -4,7 +4,7 @@ func go_to_controller_select() -> void:
 	get_tree().change_scene_to_file("res://menus/device_select.tscn")
 
 func go_to_tutorial() -> void:
-	pass
+	get_tree().change_scene_to_file("res://menus/tutorial.tscn")
 
 func quit_to_desktop() -> void:
 	get_tree().quit()
