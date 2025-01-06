db495a86b2d9781a274774e417dfb758cf69c917
Eero Häihälä <eero.haihala@gmail.com>
2025-01-06 14:01:42 +0200

    Prevent starting game with no players

--- godot-project/menus/device_select.gd
+++ godot-project/menus/device_select.gd
@@ -2,8 +2,9 @@ extends Control
 
 var devices = {-1: false}
 
+@export var error_message: Node
+
 func _ready() -> void:
-	
 	for controller in Input.get_connected_joypads():
 		devices[controller] = false
 
@@ -21,8 +22,12 @@ func start_game() -> void:
 	for dev in devices:
 		if devices[dev]:
 			players.push_back(dev)
-	Globals.player_handles = players
-	get_tree().change_scene_to_file("res://level/world.tscn")
+	
+	if players.size() == 0 || players.size() > 4:
+		error_message.visible = true
+	else:
+		Globals.player_handles = players
+		get_tree().change_scene_to_file("res://level/world.tscn")
 
 func return_to_main_menu() -> void:
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
